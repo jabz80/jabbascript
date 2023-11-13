6cb3d15e35594e4889b08de04f295d7498819e23
Milanych <vadimzotov.uk@gmail.com>
2023-11-13 16:27:45 +0000

    fight page

--- client/layouts/fight.jsx
+++ client/layouts/fight.jsx
@@ -4,13 +4,13 @@ import { Header, Footer } from "../components";
 function index() {
   
   return (
-    <>
+    <div className="fight-page d-flex flex-column">
       <Header />
 
         <Outlet />
 
-      <Footer />
-    </>
+      {/* <Footer /> */}
+    </div>
   );
 }
 

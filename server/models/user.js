b1e639baeb6b02d0edd6f8518c5bbd8ede70ef30
JJatau <jjatau14@outlook.com>
2023-11-13 12:24:35 +0000

    changes made to model and sql file

--- server/models/user.js
+++ server/models/user.js
@@ -1,61 +1,72 @@
-const db = require('../database/connect')
+const db = require('../database/connect');
 
 class User {
-    constructor({user_id, username, password}){
-        this.user_id = user_id;
-        this.username = username;
-        this.password = password;
+  constructor({ user_id, username, password, email }) {
+    this.user_id = user_id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
 
+  static async checkUsername(username) {
+    const response = await db.query('SELECT * FROM user WHERE username = $1;', [
+      username,
+    ]);
+
+    if (response.rows.length != 1) {
+      throw new Error('Unable to locate username!');
     }
+    return new User(response.rows[0]);
+  }
 
-    static async checkUsername (username){
-        const response = await db.query("SELECT * FROM user WHERE username = $1;", [username])
+  static async create(data) {
+    try {
+      const { username, password, email } = data;
+    //   console.log(username, password, email);
 
-        if (response.rows.length != 1){
-            throw new Error("Unable to locate username!")
-        }
-        return new User(response.rows[0])
+      let response = await db.query(
+        'INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING user_id;',
+        [username, password, email]
+      );
+
+      if (response.rows.length === 0) {
+        throw new Error('Failed to create username');
+      }
+      return response.rows[0];
+    } catch (err) {
+      if (
+        err.message ===
+        'duplicate key value violates unique constraint "user_username_key"'
+      ) {
+        throw new Error('Username already exists');
+      } else {
+        throw new Error('Failed to create username');
+      }
     }
+  }
 
-    static async create(data) {
-        try{
-        const {username, password} = data;
-            
-        let response = await db.query("INSERT INTO user (username, password) VALUES ($1, $2) RETURNING user_id;", [username, password]) 
+  static async getOneById(id) {
+    const response = await db.query('SELECT * FROM user WHERE user_id = $1', [
+      id,
+    ]);
 
-        if (response.rows.length === 0) {
-            throw new Error ("Failed to create username")
-        }
-        return response.rows[0]
-        } catch(err){ 
-            if (err.message === 'duplicate key value violates unique constraint "user_username_key"'){
-                throw new Error('Username already exists')
-            } else {
-                throw new Error('Failed to create username')
-            }
-        }
-
+    if (response.rows.length != 1) {
+      throw new Error('Unable to locate user');
     }
+    return new User(response.rows[0]);
+  }
 
-    static async getOneById(id){
-        const response = await db.query("SELECT * FROM user WHERE user_id = $1", [id])
-        
-        if (response.rows.length != 1){
-            throw new Error("Unable to locate user")
-        }
-        return new User(response.rows[0])
+  static async getOneByToken(token) {
+    const responseToken = await db.query(
+      'SELECT user_id FROM Token WHERE token = $1',
+      [token]
+    );
+    if (responseToken.rows.length != 1) {
+      throw new Error('Unable to locate user');
     }
-
-    static async getOneByToken(token){
-        const responseToken = await db.query("SELECT user_id FROM Token WHERE token = $1", [token])
-        if (responseToken.rows.length != 1){
-            throw new Error ("Unable to locate user")
-        }
-        const user = await User.getOneById((await responseToken).rows[0].user_id)
-        return user
-    }
-
-    
+    const user = await User.getOneById((await responseToken).rows[0].user_id);
+    return user;
+  }
 }
 
-module.exports = User
+module.exports = User;

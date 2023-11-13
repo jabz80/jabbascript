e243d3ec41e928933847c423e0ddea53a47a3fb1
cstylianou98 <cstylianou98@gmail.com>
2023-11-13 14:36:56 +0200

    fixed sql

--- server/models/user.js
+++ server/models/user.js
@@ -1,69 +1,28 @@
-const db = require('../database/connect');
+const db = require('../database/connect')
 
 class User {
-<<<<<<< HEAD
     constructor({user_id, username, password, email}){
         this.user_id = user_id;
         this.username = username;
         this.password = password;
         this.email = email;
-=======
-  constructor({ user_id, username, password, email }) {
-    this.user_id = user_id;
-    this.username = username;
-    this.password = password;
-    this.email = email;
-  }
->>>>>>> 53284df4df7cafd9e780e5732f37a50c51e85380
 
-  static async checkUsername(username) {
-    const response = await db.query('SELECT * FROM user WHERE username = $1;', [
-      username,
-    ]);
-
-    if (response.rows.length != 1) {
-      throw new Error('Unable to locate username!');
     }
-    return new User(response.rows[0]);
-  }
 
-<<<<<<< HEAD
     static async checkUsername (username){
         const response = await db.query("SELECT * FROM users WHERE username = $1;", [username])
-=======
-  static async create(data) {
-    try {
-      const { username, password, email } = data;
-    //   console.log(username, password, email);
->>>>>>> 53284df4df7cafd9e780e5732f37a50c51e85380
 
-      let response = await db.query(
-        'INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING user_id;',
-        [username, password, email]
-      );
-
-      if (response.rows.length === 0) {
-        throw new Error('Failed to create username');
-      }
-      return response.rows[0];
-    } catch (err) {
-      if (
-        err.message ===
-        'duplicate key value violates unique constraint "user_username_key"'
-      ) {
-        throw new Error('Username already exists');
-      } else {
-        throw new Error('Failed to create username');
-      }
+        if (response.rows.length != 1){
+            throw new Error("Unable to locate username!")
+        }
+        return new User(response.rows[0])
     }
-  }
 
-<<<<<<< HEAD
     static async create(data) {
         try{
         const {username, password, email} = data;
             
-        let response = await db.query("INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING user_id;", [username, password, email]) 
+        let response = await db.query("INSERT INTO users'(username, password, email) VALUES ($1, $2, $3) RETURNING user_id;", [username, password, email]) 
 
         if (response.rows.length === 0) {
             throw new Error ("Failed to create username")
@@ -76,20 +35,9 @@ class User {
                 throw new Error('Failed to create username')
             }
         }
-=======
-  static async getOneById(id) {
-    const response = await db.query('SELECT * FROM user WHERE user_id = $1', [
-      id,
-    ]);
->>>>>>> 53284df4df7cafd9e780e5732f37a50c51e85380
 
-    if (response.rows.length != 1) {
-      throw new Error('Unable to locate user');
     }
-    return new User(response.rows[0]);
-  }
 
-<<<<<<< HEAD
     static async getOneById(id){
         const response = await db.query("SELECT * FROM users WHERE user_id = $1", [id])
         
@@ -97,19 +45,18 @@ class User {
             throw new Error("Unable to locate user")
         }
         return new User(response.rows[0])
-=======
-  static async getOneByToken(token) {
-    const responseToken = await db.query(
-      'SELECT user_id FROM Token WHERE token = $1',
-      [token]
-    );
-    if (responseToken.rows.length != 1) {
-      throw new Error('Unable to locate user');
->>>>>>> 53284df4df7cafd9e780e5732f37a50c51e85380
     }
-    const user = await User.getOneById((await responseToken).rows[0].user_id);
-    return user;
-  }
+
+    static async getOneByToken(token){
+        const responseToken = await db.query("SELECT user_id FROM Token WHERE token = $1", [token])
+        if (responseToken.rows.length != 1){
+            throw new Error ("Unable to locate user")
+        }
+        const user = await User.getOneById((await responseToken).rows[0].user_id)
+        return user
+    }
+
+    
 }
 
-module.exports = User;
+module.exports = User

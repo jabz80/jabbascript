fb1c041147553f95a6876138989a2cb8860c3aeb
JJatau <jjatau14@outlook.com>
2023-11-13 16:41:00 +0000

    updated new models, routes and controllers

--- server/middleware/authenticator.js
+++ server/middleware/authenticator.js
@@ -1,20 +1,23 @@
-const Token = require("../models/token");
+const Token = require('../models/token');
 
 async function authenticator(req, res, next) {
-    try {
-        const userToken = req.headers["authorization"];
+  try {
+    const userToken = req.headers['authorization'];
+    console.log(userToken);
 
-        if (userToken == "null") {
-            throw new Error("User not authenticated.");
-        } else {
-            const validToken = await Token.getOneByToken(userToken);
+    if (userToken == 'null') {
+      throw new Error('User not authenticated.');
+    } else {
+      const validToken = await Token.getOneByToken(userToken);
 
-            next();
-        }
-
-    } catch (err) {
-        res.status(403).json({ error: err.message });
+      req.user = validToken;
+      
+      next();
     }
+    
+  } catch (err) {
+    res.status(403).json({ error: err.message });
+  }
 }
 
-module.exports = authenticator
+module.exports = authenticator;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,17 @@
-const { Router } = require('express')
+const { Router } = require('express');
 
-const authenticator = require('../middleware/authenticator')
-const userController = require('../controllers/user')
+const authenticator = require('../middleware/authenticator');
+const userController = require('../controllers/user');
 
 const userRouter = Router();
 
-userRouter.post("/register", userController.register)
-userRouter.post("/login", userController.logIn)
-userRouter.delete("/logout", userController.logOut)
-userRouter.get("/",authenticator,(req,res)=>{res.sendStatus(200)})
-userRouter.get("/token", userController.findByToken)
+userRouter.post('/register', userController.register);
+userRouter.post('/login', userController.logIn);
+userRouter.delete('/logout', userController.logOut);
+userRouter.get('/', authenticator, (req, res) => {
+  res.sendStatus(200);
+});
+userRouter.get('/token', userController.findByToken);
+userRouter.patch('/update', userController.updateUser);
 
-module.exports = userRouter
+module.exports = userRouter;

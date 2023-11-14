edfb4cbcd84b4f682f401e50f6b9974c13ced8db
cstylianou98 <cstylianou98@gmail.com>
2023-11-14 18:42:20 +0200

    Initiated backend testing files

--- client/pages/Leaderboard/index.jsx
+++ client/pages/Leaderboard/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
-import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:3000');
+
 function index() {
   return <div>index</div>;
 }

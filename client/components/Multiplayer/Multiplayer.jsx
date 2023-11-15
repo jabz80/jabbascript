ba80f8e908e772adaf791e24bdf8310b9e0d8b51
JJatau <jjatau14@outlook.com>
2023-11-15 09:43:34 +0000

    added submit answers logic

--- client/components/Multiplayer/Multiplayer.jsx
+++ client/components/Multiplayer/Multiplayer.jsx
@@ -4,10 +4,15 @@ import io from 'socket.io-client';
 
 const socket = io.connect('http://localhost:3000');
 
-
-
-
 export default function Gamepage() {
+  const [fetchedQuestions, setFetchedQuestions] = useState([]);
+  const [rooms, setRooms] = useState({});
+
+  //
+  const submitAnswer = (answer) => {
+    socket.emit('submit_answer', { roomNumber, userId: socket.id, answer });
+  };
+
   const joinRoom = () => {
     socket.emit('join_room');
   };
@@ -16,17 +21,94 @@ export default function Gamepage() {
     socket.emit('jermaine');
   };
 
+  // socket.on('updated_rooms', (updatedRooms) => {
+  //   console.log('Received rooms updated: ', updatedRooms);
+  // });
+
+  // useEffect(() => {
+  //   const handleBeforeUnload = () => {
+  //     leaveRoom();
+  //   };
+
+  //   window.addEventListener('beforeunload', handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener('beforeunload', handleBeforeUnload);
+  //   };
+  // }, []);
+
+  // Listen for the 'updated_rooms' event to receive updated rooms information
   useEffect(() => {
-    const handleBeforeUnload = () => {
-      leaveRoom();
+    const handleUpdatedRooms = (updatedRooms) => {
+      console.log('Received rooms updated: ', updatedRooms);
+
+      // Extract the rooms information and use it
+      const { rooms } = updatedRooms;
+      //Set rooms state variable
+      setRooms(rooms);
+      Object.keys(rooms).forEach((roomNumber) => {
+        const usersInRoom = rooms[roomNumber];
+
+        if (usersInRoom.length === 2) {
+          console.log(
+            `Room ${roomNumber} has two players. Emitting display_question.`
+          );
+          socket.emit('display_question', {
+            roomNumber,
+          });
+        }
+      });
     };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
+    socket.on('updated_room', handleUpdatedRooms);
 
     return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
+      socket.off('updated_room', handleUpdatedRooms);
     };
-  }, []); 
+  }, [rooms]);
+
+  useEffect(() => {
+    const handleQuestions = (data) => {
+      const usersInRoom = rooms[data.roomNumber];
+
+      if (usersInRoom && usersInRoom.length === 2) {
+        console.log(
+          `Received Questions in room ${data.roomNumber}: `,
+          data.questions
+        );
+        setFetchedQuestions((prevQuestions) => [
+          ...prevQuestions,
+          ...data.questions,
+        ]);
+        console.log(fetchedQuestions);
+      } else {
+        console.log(
+          `Not enough users in room ${data.roomNumber} to display questions`
+        );
+      }
+      console.log(fetchedQuestions);
+    };
+
+    console.log(fetchedQuestions);
+
+    socket.on('receive_question', handleQuestions);
+
+    return () => {
+      socket.off('receive_question', handleQuestions);
+    };
+  }, [rooms]);
+  console.log(fetchedQuestions);
+
+  useEffect(() => {
+    const handleDisplayAnswers = ({ user1Answer, user2Answer }) => {
+      console.log('User1Answer', user1Answer);
+      console.log('User2Answer', user2Answer);
+
+      //Implement logic to display answers to users
+    };
+
+    socket.on('display_answers', handleDisplayAnswers);
+  }, []);
 
   return (
     <div>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
 const { app, io, server } = require('./app');
 const port = process.env.PORT;
 
 const users = {};
 const rooms = {};
+const sentQuestions = {};
+const userAnswers = {};
+
+const getQuestions = async () => {
+  try {
+    const res = await fetch(`https://jabbascript-api.onrender.com/battle`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 let roomNumber = 1;
-console.log(rooms);
 
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
@@ -23,15 +35,53 @@ io.on('connection', (socket) => {
     rooms[roomNumber] = rooms[roomNumber] || [];
     rooms[roomNumber].push(socket.id);
     console.log(rooms);
-  });
-  // Broadcast logic
 
-  // Disconnection logic
+    io.emit('updated_room', { users, rooms }); // passes rooms
+  });
+
+  // Broadcast logic
+  socket.on('display_question', async ({ roomNumber }) => {
+    try {
+      if (rooms[roomNumber] && !sentQuestions[roomNumber]) {
+        const questions = await getQuestions();
+        // console.log(`Questions emitted to room ${roomNumber}: `, questions);
+        io.to(roomNumber).emit('receive_question', { roomNumber, questions });
+
+        // Mark that questions have been sent for this room
+        sentQuestions[roomNumber] = true;
+      } else {
+        console.error('User is not in a valid room');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // User answers logic
+
+  socket.on('submit_answer', ({ roomNumber, userId, answer }) => {
+    // Store users answers in the room
+    userAnswers[roomNumber] = userAnswers[roomNumber] || {};
+    userAnswers[roomNumber][userId] = answer;
+
+    //Check if both users answered
+    const answersInRoom = userAnswers[roomNumber];
+    const userIds = Object.keys(answersInRoom);
+    if (userIds.length === 2) {
+      //Both users have answered, emit the answers to both users
+      const user1Answer = answersInRoom[userIds[0]];
+      const user2Answer = answersInRoom[userIds[1]];
+
+      io.to(roomNumber).emit('display_answers', { user1Answer, user2Answer });
+
+      delete userAnswers[roomNumber];
+    }
+  });
 
   // Disconnection logic
   socket.on('disconnect', () => {
     console.log('User disconnected');
-    
+
     const userRoomNumber = users[socket.id];
     if (userRoomNumber) {
       const userIndex = rooms[userRoomNumber].indexOf(socket.id);
@@ -40,15 +90,21 @@ io.on('connection', (socket) => {
         rooms[userRoomNumber].splice(userIndex, 1);
         console.log(rooms);
       }
+
+      sentQuestions[roomNumber] = false;
+      delete users[socket.id];
     }
 
     socket.leave(userRoomNumber);
+
+    // socket.broadcast.emit('user-disconnected', users[socket.id]);
+    // delete users[socket.id];
 
     // socket.broadcast.emit('user-disconnected', users[socket.id]); // use this same function on frontend to handle user leaving game page
     // delete users[socket.id];
   });
 
-  // Additional logic to handle 'jermaine' event
+  // Manual disconnection logic
   socket.on('jermaine', () => {
     console.log('Received jermaine event on the server');
     const userRoomNumber = users[socket.id];
@@ -58,6 +114,9 @@ io.on('connection', (socket) => {
       if (userIndex !== -1) {
         rooms[userRoomNumber].splice(userIndex, 1);
       }
+
+      sentQuestions[roomNumber] = false;
+      delete users[socket.id];
     }
 
     socket.leave(userRoomNumber);

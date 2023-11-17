8d4ec648821b5f70296ea924630b8eedbcf8493b
Milanych <vadimzotov.uk@gmail.com>
2023-11-17 14:09:06 +0000

    changes

--- server/app.js
+++ server/app.js
@@ -19,10 +19,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
-}); 
+});
 
 // Middleware
 app.use(express.json());

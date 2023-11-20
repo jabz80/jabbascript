2c7d2c64de0d579385788094356480fe9342853f
jabz80 <jabzhtc@gmail.com>
2023-11-20 11:42:41 +0000

    frontend testing initial commit

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer({footerBg}) {
   return (
-    <footer style={{backgroundColor: footerBg}} className='text-white w-100 d-flex py-3 justify-content-center'>2023 © JabbaScript™</footer>
+    <footer role="footer" style={{backgroundColor: footerBg}} className='text-white w-100 d-flex py-3 justify-content-center'>2023 © JabbaScript™</footer>
   )
 }
 

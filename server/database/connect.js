469678b2671cbeec303521fdcf3ad2004b6658bf
cstylianou98 <cstylianou98@gmail.com>
2023-11-22 17:29:59 +0200

    Heroku commits 2.0

--- server/database/connect.js
+++ server/database/connect.js
@@ -5,7 +5,7 @@ require('dotenv').config(); // needed for integration testing
 // Connect to the database
 
 const db = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 // Exporting db

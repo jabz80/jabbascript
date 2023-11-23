54e3177ecba22068ce7c384b0c6772df44b01fcf
cstylianou98 <cstylianou98@gmail.com>
2023-11-23 11:33:25 +0200

    Database questions updated

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -76,20 +76,16 @@ VALUES (
     '10'
   ),
   (
-    'Use a for loop and print out the numbers from 1 to 5',
-    '1\n2\n3\n4\n5'
-  ),
-  (
-    'Create a list called fruits and assign the items "apple, banana, orange". Then use a for loop to print each item in the list."',
-    'apple\nbanana\norange'
-  ),
-  (
-    'Create a list called fruits and assign the items \"apple, banana, orange\". Use the append function and add \"grape\" to the listThen use a for loop to print each item in the list.',
-    'apple\nbanana\norange\ngrape'
-  ),
-  (
     'Create a function that adds two numbers and returns the result. Then call the function with add the numbers 6 and 7 together. Then print the results.',
     '15'
+  ),
+  (
+    'Create a function that checks if a number is even or odd and returns the "Even" or "Odd". Then check your function works by testing the number 3. Then print the results.',
+    'Odd'
+  ),
+  (
+    'Create a function that takes a list of numbers as an argument and returns the average of those numbers. Then check your function works by testing the list [2,5,8,9]. Then print the results.',
+    '13'
   );
 
 INSERT INTO avatar(img_url, gender, skin_colour)
@@ -163,7 +159,7 @@ VALUES (
     1,
     'Question 1',
     'Good, now you try. Print "Hello World" for me.',
-    'print("Hello World)'
+    'print("Hello World")'
   ),
   (
     1,
